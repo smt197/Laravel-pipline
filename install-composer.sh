26dfde783dabefac718bb8b6c26c576fc4bdceb0
groupe-06 <groupe6p6@gmail.com>
2024-10-24 23:54:17 +0000

    fichier composer

--- install-composer.sh
+++ install-composer.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Met à jour et installe PHP
+sudo apt-get update
+sudo apt-get install -y php php-cli php-fpm php-json php-pdo php-mysql php-zip php-gd php-mbstring php-curl php-xml php-bcmath php-json
+
+# Vérifie l'installation de PHP
+php -v
+
+# Installation de Composer
 EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
